8f96a5f175d2c565a0ef26227faf7cd0595277e4
FelipeOliveira22 <felipe.olv@alu.ufc.br>
2025-11-11 08:41:46 -0300

    feat: create home screen with logo and play button

---
+++ LMS-Financinhas/src/pages/HomeScreen/HomeScreen.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import logo from '../../assets/LogoFinancinhas.svg'
+import './HomeScreen.style.css'
+import { Button } from '../../components/Button'
+import { useNavigate } from 'react-router-dom'
+
+const HomePage: React.FC = () => {
+    const navigate = useNavigate()
+
+    const handlePlayClick = (): void => {
+        navigate('/profile-select')
+    }
+
+    return (
+        <div className="container">
+            <img className="logoHome" src={logo} alt="Financinhas" />
+            <Button
+                onClick={handlePlayClick}
+                className="button"
+                style={{ backgroundColor: '#13BB0A', borderColor: '#00570A' }}
+            >
+                JOGAR
+            </Button>
+        </div>
+    )
+}
+
+export default HomePage
