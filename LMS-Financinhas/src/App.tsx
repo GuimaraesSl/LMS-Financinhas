01b8e5b1d14cefe2b3250003961e72de9528c681
gabrielmendes64 <gabriel10010100@gmail.com>
2025-11-12 10:24:45 -0300

    feat: add teacher login screen and its styles

--- LMS-Financinhas/src/App.tsx
+++ LMS-Financinhas/src/App.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css'
 import HomePage from './pages/HomeScreen/HomeScreen'
+import LoginScreen from './pages/LoginScreen/LoginScreen'
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginScreen />} />
       </Routes>
     </Router>
   )
---
+++ LMS-Financinhas/src/pages/LoginScreen/LoginScreen.tsx
@@ -0,0 +1,70 @@
+import { useState, type FC } from "react"
+import { useNavigate } from "react-router-dom"
+
+import { MdArrowBack } from 'react-icons/md'
+import InputField from "../../components/InputField/InputField"
+import logo from '../../assets/Logo-Subtitle.svg'
+
+export const LoginScreen: FC = () => {
+  const navigate = useNavigate()
+
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleLogin = async (): Promise<void> => {
+    try {
+      navigate('/config-team-room')
+    } catch (error) {
+      alert('Erro ao fazer login, tente novamente')
+      console.error('Error during sign in:', error)
+    }
+  }
+
+
+  return (
+    <div className="containerLoginScreen">
+      <header className="headerLoginScreen">
+        <MdArrowBack
+          onClick={() => navigate('/profile-select')}
+          size={45}
+          color="#000"
+          className="arrowIcon"
+        />
+        <img src={logo} className="logoLoginScreen" alt="logo" />
+      </header>
+      <main className="mainLoginScreen">
+        <form className="formLoginScreen">
+          <h2>LOGIN</h2>
+          <InputField
+            id="email"
+            name="email"
+            label="Email"
+            type="email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <InputField
+            id="senha"
+            name="senha"
+            label="Senha"
+            type="password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button
+            onClick={(e) => {
+              e.preventDefault()
+              handleLogin()
+            }}
+            className="buttonLoginScreen"
+          >
+            ENTRAR
+          </button>
+          <p onClick={() => navigate('/register')} className="registerLinkLoginScreen">
+            Ainda não tenho uma conta
+          </p>
+        </form>
+      </main>
+    </div>
+  )
+}
+
+export default LoginScreen
