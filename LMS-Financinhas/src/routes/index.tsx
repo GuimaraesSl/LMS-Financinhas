3ea966ccb1b21b6cf8bccaebeea349a28019b12f
EmanuelALemos <araujoemanuel97@gmail.com>
2025-11-18 21:46:22 -0300

    feat: add path to new screen in routes

--- LMS-Financinhas/src/routes/index.tsx
+++ LMS-Financinhas/src/routes/index.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 import { HashRouter, Routes, Route } from 'react-router-dom'
 import HomePage from '../pages/HomeScreen/HomeScreen'
 import LoginScreen from '../pages/LoginScreen/LoginScreen'
+import EditQuestionScreen from '../pages/EditQuestionScreen/EditQuestionScreen'
 
 const AppRoutes: React.FC = () => {
   return (
@@ -13,6 +14,9 @@ const AppRoutes: React.FC = () => {
 
          {/* Rota para a tela de Login */}
         <Route path="/login" element={<LoginScreen />} />
+
+        {/* Rota para a tela editar pergunta */}
+        <Route path="/edit-questions" element={<EditQuestionScreen />} />
       </Routes>
     </HashRouter>
   )
